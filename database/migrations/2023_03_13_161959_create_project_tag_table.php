447e5f8a846249bc43b286bda71f6112218bd1a7
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-13 19:38:58 +0100

    fix name table

--- database/migrations/2023_03_13_161959_create_project_tag_table.php
+++ database/migrations/2023_03_13_161959_create_project_tag_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('project_tech', function (Blueprint $table) {
+        Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
             $table->foreignId('project_id')->constrained()->onDelete('cascade');
             $table->foreignId('technology_id')->constrained()->onDelete('cascade');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('project_tech');
+        Schema::dropIfExists('project_technology');
     }
 };
